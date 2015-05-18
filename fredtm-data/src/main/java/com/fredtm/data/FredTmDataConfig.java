81f2c989fd1503544658d5e1244dd94c9e05e0f3
leonardo zapparoli <leo.zapparoli@gmail.com>
2015-05-18 12:36:55 -0300

    add procfile

--- fredtm-data/src/main/java/com/fredtm/data/FredTmDataConfig.java
+++ fredtm-data/src/main/java/com/fredtm/data/FredTmDataConfig.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.test.context.ActiveProfiles;
 
 @Configuration
 @ComponentScan(basePackages = "com.fredtm.core.model")
@@ -16,12 +14,9 @@ import org.springframework.test.context.ActiveProfiles;
 @EnableJpaRepositories(basePackages = { "com.fredtm.core.model",
 		"com.fredtm.data", "com.fredtm.data.repository" }, transactionManagerRef = "transactionManager")
 @EntityScan(basePackages = { "com.fredtm.core.model" })
-@ActiveProfiles(profiles = "dev")
-@Import(value = DBConfig.class)
 public class FredTmDataConfig {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FredTmDataConfig.class,
-				"--spring.profiles.active=dev");
+		SpringApplication.run(FredTmDataConfig.class, args);
 	}
 }
